ee22ba76528d94d12c79a672f3aff40a435258d2
isilg <isilavunduk@gmail.com>
2020-03-05 13:50:09 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -4,9 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class JumpToTheNewWindow {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
@@ -19,5 +21,17 @@ public class JumpToTheNewWindow {
         //based on window handle, we can switch in between windiws
         String windowHandle = driver.getWindowHandle();
         System.out.println(windowHandle);
+
+        Set<String> windowHandles = driver.getWindowHandles();   //getting all of the current open window id's.
+                                                                 //Return type of the method is Set. Set doesnt allow duplicates
+        System.out.println(windowHandles);
+        System.out.println("BEFORE SWITCH: "+driver.getCurrentUrl());
+        //since we know id of original window, we can say switch to sth else that is not equals to old window id
+        for ( String windowId : windowHandles){
+            if (!windowId.equals(windowHandle)){        //if it is not an old window, then switch
+                driver.switchTo().window(windowId);     //to jump to the new window
+            }
+        }
+        System.out.println("AFTER SWITCH: "+driver.getCurrentUrl());
     }
 }
