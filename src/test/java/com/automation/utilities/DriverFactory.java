8dcc5837a517c49ce5812a4adb3df40d93703cef
isilg <isilavunduk@gmail.com>
2020-03-06 11:56:28 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -0,0 +1,29 @@
+package com.automation.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    /**
+     * utilities package stores reusable methods.
+     * Here, we're creating a static method it's return type is WebDriver
+     * This method will take one parameter and base on the browserName
+     * it will give us(return) WebDriver object
+     */
+
+    public static WebDriver createADriver(String browserName){
+        if (browserName.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        }else{
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
+
+    }
+
+
+
+}
