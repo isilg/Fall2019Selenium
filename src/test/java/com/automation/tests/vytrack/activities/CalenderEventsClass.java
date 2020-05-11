1c9cf16aa5e2ea9baca1aaf55cbcf165cb167985
isilg <isilavunduk@gmail.com>
2020-05-11 11:27:33 -0400

    activities

--- src/test/java/com/automation/tests/vytrack/activities/CalenderEventsClass.java
+++ src/test/java/com/automation/tests/vytrack/activities/CalenderEventsClass.java
@@ -111,9 +111,7 @@ public class CalenderEventsClass {
         String expectedTime = LocalTime.now(ZoneId.of("GMT-7")).format(DateTimeFormatter.ofPattern("h:m a"));
         String actualTime =driver.findElement(startTimeBy).getAttribute("value");
         Assert.assertEquals(actualTime, expectedTime);
-
     }
-
 
     @AfterMethod
     public void teardown(){
