12030769e1822743c2e8dea612dd2f92f27bb63f
isilg <isilavunduk@gmail.com>
2020-05-15 15:02:31 -0400

    day12

--- src/test/java/com/automation/tests/day12/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12/ExplicitWait.java
@@ -1,11 +1,9 @@
 package com.automation.tests.day12;
 
 import com.automation.utilities.DriverFactory;
-import org.apache.commons.math3.analysis.function.Exp;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
